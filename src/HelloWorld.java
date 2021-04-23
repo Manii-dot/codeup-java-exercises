41895d4e87c9d5fe1ac71762b88699f8a86686a9
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-23 11:54:31 -0500

    feat:exercise in progress

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,13 +8,29 @@ public class HelloWorld {
 
         // 2 Create a String variable named myString and assign a string value to it, then print the variable out to the console.
         String myString = "Hello";
+//        myString = 'c';
+//        myString = 3.14159;
         System.out.println(myString);
 
         // 3 Change your code to assign a character value to myString. What do you notice?
+            //When trying to reasign the character value it would not accept it.
 
+        // 4 Change your code to assign the value 3.14159 to myString. What happens?
+        // Same thing as the last exercise it would not accept it.
 
+        // 5 Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+          long myNumber;
+//          myNumber = 3.14;
+            //myNumber = 123L;
+            myNumber = 123;
+//        System.out.println(myNumber);
+        //java: variable myNumber might not have been initialized
 
+        //6 Change your code to assign the value 3.14 to myNumber. What do you notice?
+        // it gives an error of incompatible types: possible lossy conversion from double to long
 
+        //7 Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+            // the value is supposed to be a float instead of long or int for 3.14
 
 
 
