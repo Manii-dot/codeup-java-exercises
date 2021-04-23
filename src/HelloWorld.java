6d4a5adaeede17292b2e90ef7b2b56d04dcf6f8d
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-23 12:01:50 -0500

    feat:exercise 8 complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -19,10 +19,10 @@ public class HelloWorld {
         // Same thing as the last exercise it would not accept it.
 
         // 5 Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
-          long myNumber;
-//          myNumber = 3.14;
+          //long myNumber;
+            //myNumber = 3.14;
             //myNumber = 123L;
-            myNumber = 123;
+            //myNumber = 123;
 //        System.out.println(myNumber);
         //java: variable myNumber might not have been initialized
 
@@ -30,9 +30,15 @@ public class HelloWorld {
         // it gives an error of incompatible types: possible lossy conversion from double to long
 
         //7 Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
-            // the value is supposed to be a float instead of long or int for 3.14
 
 
+        // 8 Change your code to assign the value 123 to myNumber.
+        //
+        //Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+        // the value is supposed to be a float instead of long or int for 3.14
 
+        //9 Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+        //float myNumber = 3.14;    there is an error that happened when trying to get it to work.
+        double myNumber = 3.14;     // assigning the double as the declaration is the fix for the number range.
     }
 }
