809d189c219014e6dcf57a1904c1a778a3e626a7
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-23 19:24:26 -0500

    feat:lecture in progress

--- src/ConsoleIOLecture.java
+++ src/ConsoleIOLecture.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ConsoleIOLecture {
     public static void main(String[] args){
         System.out.println("Console IO Lecture");
@@ -10,17 +12,32 @@ public class ConsoleIOLecture {
 
         // is equivalent to this:
         System.out.print("here\n");
-        System.out.print("here\n");
+        System.out.print("there\n");
 
         //without the newline characters, print output this:
         System.out.print("here");
-        System.out.print("here");
+        System.out.print("there");
 
         // to concatenate, just like JS:
         String firstName = "Emmanuel";
         String lastName = "Stephen";
         System.out.println(firstName + " " + lastName);
 
+
+        // ===================================User Input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userFirstName = sc.next();
+        System.out.printf("Nice to meet you, %s\n", userFirstName);
+
+        System.out.println("How old are you?");
+        int age = sc.nextInt();
+        System.out.printf("Good to hear you are %d years old.\n", age);
+
+        sc.nextLine();
+        System.out.println("Favorite Quote:");
+        String favoriteQuote = sc.nextLine();
+        System.out.printf("Funny, \"%s\" is my favorite quote too.", favoriteQuote);
 
     }
 }
