fe3b08ea92fbb64b04f321b5db34063f50181dd7
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-26 09:55:16 -0500

    feat:Console Input and Output Scanner  exercise 1 complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
 
         //1
         double pi = 3.14159;
-        System.out.format("%.2f%n",pi);
+        System.out.format("The value of pi is approximately %.2f%n.",pi);
+
+        //Explore the Scanner Class
+        //1
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What number do you want to enter here?");
+        int number = sc.nextInt();
+        System.out.printf("You had enter %d", number);
+        //If the user input a string there is an error that happen.
+
+
+
     }
 }
