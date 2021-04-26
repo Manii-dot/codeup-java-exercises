b2c041747ae693677ff4c88f9bda43fee4dd9139
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-26 15:02:36 -0500

    feat: Started the control Flow exercise 1 and 2 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,35 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+        //1 LOOP BASICS
+        //a) while
+        int i = 5;
+        while (i <= 15){
+            System.out.println("the current value of i is " + i);
+            i++;
+        }
 
+        //b) do While
 
+        //1b
+        int a = 0;
+        do{
+            a += 2;
+            System.out.println( "a is "+a);
+        }while(a<100);
 
+        //2b
+        int b = 105;
+        do{
+            b = b - 5;
+            System.out.println("b is " +b);
+        }while(b>-10);
 
+        //3b
+        long c = 2;
+        do{
+            c = c*c;
+            System.out.println("Square of c is "+c);
+        }while(c<65536);
     }
 }
