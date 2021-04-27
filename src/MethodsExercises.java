f60d2968c8369d18e9ca909ce6ae868965cea181
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-27 15:38:35 -0500

    feat:Methods Exercises 1 done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,6 +4,8 @@ public static void main(String[] args){
     System.out.println(Subtraction(2,2));
     System.out.println(Multiplication(2,2));
     System.out.println(Division(2,2));
+    System.out.println(Modulus(0,2));
+    System.out.println(LoopMultiplication(5,2));
 
     }
 
@@ -15,19 +17,36 @@ public static void main(String[] args){
     }
 
     public static double Subtraction(double num1, double num2){
-        double Subtraction = num1 - num2;
-        return Subtraction;
+        double subtraction = num1 - num2;
+        return subtraction;
     }
 
     public static double Multiplication(double num1, double num2){
-        double Multiplication = num1 * num2;
-        return Multiplication;
+        double multiplication = num1 * num2;
+        return multiplication;
     }
 
     public static double Division(double num1, double num2){
-        double Division = num1 / num2;
-        return Division;
+        double division = num1 / num2;
+        return division;
     }
+
+    public static double Modulus(double num1, double num2){
+        double modulus = num1 % num2;
+        return modulus;
+        //If you try to divide by zero the reurn will be NaN
+    }
+    // NEED MORE UNDERSTANDING WITH THAT
+    public static double LoopMultiplication(double num1, double num2){
+        int result = 0;
+        while(num1 > 0){
+            result += num2;
+            num1--;
+        }
+        return result;
+    }
+
+
 
 
 
