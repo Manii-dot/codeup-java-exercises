1fffca3665ffb38599ee1cbd2b0da5be89cf41a8
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-27 14:45:12 -0500

    feat:MethodsExercises 1 halfway done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,35 @@
 public class MethodsExercises {
 public static void main(String[] args){
+    System.out.println(Addition(2,2));
+    System.out.println(Subtraction(2,2));
+    System.out.println(Multiplication(2,2));
+    System.out.println(Division(2,2));
 
     }
+
+// 1 Basic Arithmetic
+    // a)
+    public static double Addition(double num1, double num2){
+    double addition = num1 + num2;
+    return addition;
+    }
+
+    public static double Subtraction(double num1, double num2){
+        double Subtraction = num1 - num2;
+        return Subtraction;
+    }
+
+    public static double Multiplication(double num1, double num2){
+        double Multiplication = num1 * num2;
+        return Multiplication;
+    }
+
+    public static double Division(double num1, double num2){
+        double Division = num1 / num2;
+        return Division;
+    }
+
+
+
 
 }
