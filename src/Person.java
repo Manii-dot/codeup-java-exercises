0af1b8c77516ccfe18549b34d583de328c4fb895
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-02 18:22:41 -0500

    feat: this exercise is incomplete

--- src/Person.java
+++ src/Person.java
@@ -1,34 +1,42 @@
 public class Person {
-//    public String firstName;
-//    public String lastName;
-//
-//    public String sayHello() {
-//        return String.format("Hello from %s %s!", firstName, lastName);
-//    }
-//
-//    public static void main(String[] args) {
-//        Person rick = new Person();
-//        rick.firstName = "Rick";
-//        rick.lastName = "Sanchez";
-//        System.out.println(rick.sayHello());
-//        // prints "Hello from Rick Sanchez!"
-//    }
+
 private String name;
 
     public String getName(){
 //TODO: return the person's name
-
+Person Manii = new Person();
+return Manii.name = "Emnmanuel Stephen";
     }
 
     public void setName(String name){
 //TODO: change the name property to the passed value
-
+      return name;
     }
     public void sayHello(){
 //TODO: print a message to the console using the person's name
-
+        System.out.printf("Hello from %s!", name);
     }
 
+public static void main(String[] args){
+
+}
+
+    Person person1 = new Person("John");
+    Person person2 = new Person("John");
+System.out.println(person1.getName().equals(person2.getName()));
+System.out.println(person1 == person2);
 
 
+//    erson person1 = new Person("John");
+//    Person person2 = person1;
+//System.out.println(person1 == person2);
+//
+//
+//    Person person1 = new Person("John");
+//    Person person2 = person1;
+//System.out.println(person1.getName());
+//System.out.println(person2.getName());
+//person2.setName("Jane");
+//System.out.println(person1.getName());
+//System.out.println(person2.getName());P
 }
