cf02d5a4ff909fc233034dd4ee43ecc80dff951a
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-03 10:40:08 -0500

    Feat:exercise 1 done

--- src/Person.java
+++ src/Person.java
@@ -1,33 +1,36 @@
 public class Person {
 
-private String name;
+    private String name;
 
-    public String getName(){
+    public Person(String personName) {
+        this.name = personName;
+    }
+
+        public String getName () {
 //TODO: return the person's name
-Person Manii = new Person();
-return Manii.name = "Emnmanuel Stephen";
-    }
+            return name;
+        }
 
-    public void setName(String name){
+        public void setName (String name){
 //TODO: change the name property to the passed value
-      return name;
-    }
-    public void sayHello(){
+            this.name = name;
+        }
+        public void sayHello () {
 //TODO: print a message to the console using the person's name
-        System.out.printf("Hello from %s!", name);
-    }
+            System.out.printf("Hello from %s!", name);
+        }
 
-public static void main(String[] args){
-
-}
-
-    Person person1 = new Person("John");
-    Person person2 = new Person("John");
-System.out.println(person1.getName().equals(person2.getName()));
-System.out.println(person1 == person2);
+        public static void main (String[]args){
 
 
-//    erson person1 = new Person("John");
+
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+
+//    Person person1 = new Person("John");
 //    Person person2 = person1;
 //System.out.println(person1 == person2);
 //
@@ -39,4 +42,5 @@ System.out.println(person1 == person2);
 //person2.setName("Jane");
 //System.out.println(person1.getName());
 //System.out.println(person2.getName());P
+        }
 }
---
+++ src/util/Input.java
@@ -0,0 +1,37 @@
+//package util;
+//
+//import java.util.Scanner;
+//
+//public class Input {
+//
+//    private Scanner scanner = new Scanner(System.in);
+//
+//    public String getString(){
+//
+//    }
+//
+//    public boolean yesNo(){
+//    if( getString().equals("y") || getString().equals("yes")){
+//        return true;
+//        }else {
+//        return false;
+//        }
+//    }
+//    }
+//
+//    public int getInt(int min, int max){
+//
+//    }
+//
+//    public int getInt(){
+//
+//    }
+//
+//    public double getDouble(double min, double max){
+//
+//    }
+//
+//    public double getDouble(){
+//
+//    }
+//}
