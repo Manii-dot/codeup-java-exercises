9f250c0a904aab9bddb9b432d361d97bc9521625
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-04 10:46:09 -0500

    feat: number 2 is completed

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,6 @@
 public class ServerNameGenerator {
 
-    static String[] adjectives = {"mad", "angry","moody", "sad", "hurt", "brave", "furious", "fast", "viscious", "depressed"};
+    static String[] adjectives = {"mad", "angry","moody", "sad", "painful", "brave", "furious", "fast", "viscious", "depressed"};
     static String[] nouns = {"love", "wife", "husband", "son", "daughter", "baby", "grandparent", "marriage", "life", "union"};
 
     //method that will return a random element from an array of strings
@@ -10,6 +10,13 @@ public class ServerNameGenerator {
     }
 
     //Adding a main method
+    public static void main(String[] args){
 
+        String adj = randomElement(adjectives);
+        String nou = randomElement(nouns);
+        System.out.println("Here is your server name:");
+        System.out.printf("%s-%s", adj, nou);
+
+    }
 
 }
