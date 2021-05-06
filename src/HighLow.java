1eb427e0533552553789620571c4fd13240d9be3
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-05 20:09:56 -0500

    feat:exercise is done without bonus

--- src/HighLow.java
+++ src/HighLow.java
@@ -5,23 +5,27 @@ public class HighLow {
 randomNumberGuess();
     }
     public static void randomNumberGuess(){
-        //Set up the random number
-    int random = (int)(Math.random()*100+1);
-
-    // User input
-    System.out.println("Pick a number form 1 to 100");
-    Scanner scanner = new Scanner(System.in);
-    int userGuess = scanner.nextInt();
     Boolean Guesses = true;
     do{
+        //Set up the random number
+        int random = (int)(Math.random()*100+1);
+
+        // User input
+        System.out.println("Pick a number form 1 to 100");
+        Scanner scanner = new Scanner(System.in);
+        int userGuess = scanner.nextInt();
+
         if(userGuess<random){
             System.out.println("You need to go HIGHER");
+            Guesses= true;
         }else if(userGuess>random){
             System.out.println("You need to go LOWER");
+            Guesses=true;
         }else if(userGuess == random){
             System.out.println("GOOD GUESS");
             Guesses = false;
         }
+
 
     }while(Guesses);
 
