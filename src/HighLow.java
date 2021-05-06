f6a47dd637cfc359f514c76416d65d0fa93a3b58
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-05 20:00:24 -0500

    feat:halfway done with high and low, cant stop infinite loop

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class HighLow {
     public static void main(String[] args) {
-
+randomNumberGuess();
     }
+    public static void randomNumberGuess(){
+        //Set up the random number
+    int random = (int)(Math.random()*100+1);
 
-    public static void randomNumber(){
+    // User input
+    System.out.println("Pick a number form 1 to 100");
+    Scanner scanner = new Scanner(System.in);
+    int userGuess = scanner.nextInt();
+    Boolean Guesses = true;
+    do{
+        if(userGuess<random){
+            System.out.println("You need to go HIGHER");
+        }else if(userGuess>random){
+            System.out.println("You need to go LOWER");
+        }else if(userGuess == random){
+            System.out.println("GOOD GUESS");
+            Guesses = false;
+        }
+
+    }while(Guesses);
+
+
 
     }
 
