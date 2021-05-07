5dd8a2575c59377e33e45dc4dc0c78b6bfdf1a0c
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-07 12:09:19 -0500

    feat: Collections exercise

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    public static void printEachElement(ArrayList<String> strings) {
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+
+    private String name;
+    private int grades;
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+
+}
