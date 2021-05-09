51d83ba4e0b5cfeb6473de906ee9094639c1ef33
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-08 21:29:12 -0500

    feat: practice two established

---
+++ src/Practice/practice2.java
@@ -0,0 +1,94 @@
+package Practice;
+
+public class practice2 {
+
+    public class prac {
+        public static void main(String[] args){
+
+            // Given a string, return a string where
+            // for every char in the original string,
+            // there are three chars.
+
+            // multChar("The") ==> "TTThhheee"
+            // multChar("AAbb") ==> "AAAAAAbbbbbb"
+            // multChar("Hi-There") ==> "HHHiii---TTThhheeerrreee"
+
+
+
+
+
+
+
+
+            // Return the string (backwards) that is between the first and last appearance
+            // of "bert"
+            // in the given string, or return the empty string "" if there is not 2
+            // occurances of "bert" - Ignore Case
+
+            // getBert("bertclivebert") ==> "evilc"
+            // getBert("xxbertfridgebertyy") ==> "egdirf"
+            // getBert("xxBertfridgebERtyy") ==> "egdirf"
+            // getBert("xxbertyy") ==> ""
+            // getBert("xxbeRTyy") ==> ""
+
+
+
+
+
+
+            // Given a string and an int n, return a string that removes n letters from the 'middle' of the string.
+            // The string length will be at least n, and be odd when the length of the input is odd.
+
+            // nMid("Hello", 3) ==> "Ho"
+            // nMid("Chocolate", 3) ==> "Choate"
+            // nMid("Chocolate", 1) ==> "Choclate"
+
+
+
+
+
+
+            // Given a string, return the length of the largest "block" in the string.
+            // A block is a run of adjacent chars that are the same.
+            //
+            // superBlock("hoopplla") ==> 2
+            // superBlock("abbCCCddDDDeeEEE") ==> 3
+            // superBlock("") ==> 0
+
+
+
+
+
+            //given a string - return the number of times "am" appears in the String ignoring case -
+            // BUT ONLY WHEN the word "am" appears without being followed or proceeded by other letters
+            //
+            //amISearch("Am I in Amsterdam") ==> 1
+            //amISearch("I am in Amsterdam am I?") ==> 2
+            //amISearch("I have been in Amsterdam") ==> 0
+
+
+
+
+
+
+            //Given a string split the string into the individual numbers present
+            //then add each digit of each number to get a final value for each number
+            // String example = "55 72 86"
+            //
+            // "55" will = the integer 10
+            // "72" will = the integer 9
+            // "86" will = the integer 14
+            //
+            // You then need to return the highest vale
+            //
+            //largest("55 72 86") ==> 14
+            //largest("15 72 80 164") ==> 11
+            //largest("555 72 86 45 10") ==> 15
+
+
+
+
+
+        }
+    }
+}
